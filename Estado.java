84321f3a7e59e1d5ec439ed52b36fe677e554d26
Luiza <159278111+luizaqueiiroz@users.noreply.github.com>
2024-09-25 15:43:58 -0300

    primeiro commit

---
+++ Estado.java
@@ -0,0 +1,20 @@
+package animal;
+
+public class Estado {
+	private String nome;
+	private String sigla;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void SetNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla (String sigla) {
+		this.sigla = sigla;
+	}
+
+}
